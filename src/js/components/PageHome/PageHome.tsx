add8858b5ca2dd2918ec04a5e80b9cc304c33134
Tam Pham <tamtam4@gmail.com>
2021-12-09 11:28:08 -0800

    tam - incremental

--- src/js/components/PageHome/PageHome.tsx
+++ src/js/components/PageHome/PageHome.tsx
@@ -7,7 +7,7 @@ export default function PageHome() {
   return <>
     <Header page={ routes.home }/>
     <div className="page page-home">
-      <h1>Home</h1>
+      <h1>Home test</h1>
     </div>
   </>;
 }
