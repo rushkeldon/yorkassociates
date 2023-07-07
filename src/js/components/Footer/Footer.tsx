359bfe3ecee4651c762e246985ea04a90bbafb38
Keldon Rush <keldon@lightforge.lan>
2023-07-07 14:49:47 -0700

    master - incremental

--- src/js/components/Footer/Footer.tsx
+++ src/js/components/Footer/Footer.tsx
@@ -4,34 +4,36 @@ import './Footer.css';
 
 export default function Footer(){
   return <div className="footer">
-    <div className="tri">
-      <h3>Company Data</h3>
-      <div className="data">
-        <p>DUNS Number: 148976314</p>
-        <p>Core NAICS: 541330</p>
-        <p>CAGE Code: 8EBK5</p>
+    <div className="grid">
+      <div className="tri">
+        <h3>Company Data</h3>
+        <div className="data">
+          <p>DUNS Number: 148976314</p>
+          <p>Core NAICS: 541330</p>
+          <p>CAGE Code: 8EBK5</p>
+        </div>
+      </div>
+      <div className="tri">
+        <h3>Current Certifications</h3>
+        <div className="data">
+          <p>• SBA 8(a)</p>
+          <p>• SBA EDWOSB</p>
+          <p>• SBA HUBZone</p>
+        </div>
+      </div>
+      <div className="tri">
+        <h3>GDOT Qualifications</h3>
+        <div className="data">
+          <p>3.01, 3.13, 5.01, 5.02, 8.01, and 9.01</p>
+        </div>
       </div>
     </div>
-    <div className="tri">
-      <h3>Certifications</h3>
-      <div className="data">
-        <p>• SBA 8(a)</p>
-        <p>• SBA EDWOSB</p>
-        <p>• SBA HUBZone</p>
-      </div>
-    </div>
-    <div className="tri">
-      <h3>GDOT Qualifications</h3>
-      <div className="data">
-        <p>3.01, 3.13, 5.01, 5.02, 8.01, and 9.01</p>
-      </div>
-    </div>
-    <div className="tri">
+    <div className="bottom">
       <h3>Our Contact</h3>
       <div className="data">
         <p>308 E. Water Street, Bainbridge, Georgia 39817</p>
         <a href="tel:2292480141">229.248.0141</a>
-        <p>Copyright © 2022 York & Associates Engineering, Inc. All rights reserved.</p>
+        <p className="copyright">Copyright © 2022 York & Associates Engineering, Inc. All rights reserved.</p>
       </div>
     </div>
   </div>
