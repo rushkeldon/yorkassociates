912efadf38af120d7867a135f66623fd2a275db2
Keldon Rush <keldon@spiral9.com>
2025-04-03 03:04:07 +0000

    added a link

--- src/js/components/PageBidding/PageBidding.tsx
+++ src/js/components/PageBidding/PageBidding.tsx
@@ -24,6 +24,7 @@ export default function PageBidding() {
       <h3>Links</h3>
       <ul>
         <li><a target="_blank" rel="noreferrer" href={`${baseURL}doc/bainbridge_wheat_bids.pdf`}>Advertisement for Bids</a></li>
+        <li><a target="_blank" rel="noreferrer" href={`${baseURL}doc/bainbridge_wheat_plan_holders.pdf`}>Plan Holders List</a></li>
       </ul>
       <div className="hr"/>
       <h2>Cemetery Roads- Phase I</h2>
