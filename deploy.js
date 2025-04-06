6479a656b01a657a5e768dc7503b3479e1a97ded
Keldon Rush <keldon@spiral9.com>
2025-04-06 02:57:40 +0000

    updated deploy script to SFTP

--- deploy.js
+++ deploy.js
@@ -1,18 +1,31 @@
-const FtpDeploy = require('ftp-deploy');
-const ftpDeploy = new FtpDeploy();
+const Client = require('ssh2-sftp-client');
+const path = require('path');
+const sftp = new Client();
 
 const config = {
-    user: "u39440144",
-    password: "Mtvk-92jpbs-05",
-    host: "home148174042.1and1-data.host",
-    localRoot: __dirname + "/build",
-    remoteRoot: "/",
-    include: ["*", "**/*"],
-    exclude: ['.*'],
-    deleteRemote: true,
-    forcePasv: true
+  host: 'home148174042.1and1-data.host',
+  port: 22,
+  username: 'u39440144',
+  password: 'Mtvk-92jpbs-05',
 };
 
-ftpDeploy.deploy(config)
-    .then(res => console.log('Deployment finished:', res))
-    .catch(err => console.log(err));
+const localDir = path.join(__dirname, 'build');
+const remoteDir = '/';
+
+async function deploy() {
+  try {
+    await sftp.connect(config);
+    console.log('Connected to SFTP server.');
+
+    console.log(`Uploading ${localDir} to ${remoteDir}...`);
+    await sftp.uploadDir(localDir, remoteDir);
+
+    console.log('Deployment finished.');
+  } catch (err) {
+    console.error('Deployment error:', err.message);
+  } finally {
+    sftp.end();
+  }
+}
+
+deploy();
