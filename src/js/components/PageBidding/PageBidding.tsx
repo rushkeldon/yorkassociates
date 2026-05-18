c69d04f979ceaccc9ffa34c75bc276fee1d915af
tampham98 <tampham98@users.noreply.github.com>
2026-05-18 16:42:29 +0000

    Planholders Add

--- src/js/components/PageBidding/PageBidding.tsx
+++ src/js/components/PageBidding/PageBidding.tsx
@@ -27,6 +27,8 @@ export default function PageBidding() {
       <li>
       <a target="_blank" rel="noreferrer" href={`${baseURL}doc/camilla_cemeteryrd3_ad_for_bids.pdf`}>Advertisement for Bids</a>
       </li>
+      <a target="_blank" rel="noreferrer" href={`${baseURL}doc/camilla_cemeteryrd3_plan_holders.pdf`}>Plan Holders List</a>
+      </li>
       </ul>
 
       <div className="hr"/>
